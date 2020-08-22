acb6526cb4ae156949cbb3dbc48c88816ff825b0
Bence Rozsa <robence@gmail.com>
2020-08-22 14:23:35 +0200

    feat(Orders): store orders on server

--- src/ducks/ordersDuck.js
+++ src/ducks/ordersDuck.js
@@ -10,10 +10,10 @@ export default function ordersReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_ORDER:
       const newOrder = new Order(
-        new Date().toString(),
+        action.orderData.id,
         action.orderData.items,
         action.orderData.amount,
-        new Date()
+        action.orderData.date
       );
       return {
         ...state,
@@ -25,10 +25,36 @@ export default function ordersReducer(state = initialState, action) {
   }
 }
 
-export const addOrder = (cartItems, totalAmount) => ({
-  type: ADD_ORDER,
-  orderData: {
-    items: cartItems,
-    amount: totalAmount,
-  },
-});
+export const addOrder = (cartItems, totalAmount) => async (dispatch) => {
+  const date = new Date();
+  const response = await fetch(
+    'https://rn-complete-guide-a3ac3.firebaseio.com/orders/u1.json',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cartItems,
+        totalAmount,
+        date: date.toISOString(),
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error('Something went wrong!');
+  }
+
+  const resData = await response.json();
+
+  dispatch({
+    type: ADD_ORDER,
+    orderData: {
+      id: resData.name,
+      items: cartItems,
+      amount: totalAmount,
+      date,
+    },
+  });
+};
