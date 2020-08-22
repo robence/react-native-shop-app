e43f7f5688e326ffb454efed0bb9ba9fe796065a
Bence Rozsa <robence@gmail.com>
2020-08-22 14:52:46 +0200

    feat(OrdersScreen): fetch orders with loading state

--- src/ducks/ordersDuck.js
+++ src/ducks/ordersDuck.js
@@ -1,6 +1,7 @@
 import Order from '../models/order';
 
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
 
 const initialState = {
   orders: [],
@@ -8,6 +9,11 @@ const initialState = {
 
 export default function ordersReducer(state = initialState, action) {
   switch (action.type) {
+    case SET_ORDERS: {
+      return {
+        orders: action.orders,
+      };
+    }
     case ADD_ORDER:
       const newOrder = new Order(
         action.orderData.id,
@@ -24,6 +30,33 @@ export default function ordersReducer(state = initialState, action) {
       return state;
   }
 }
+export const fetchOrders = () => async (dispatch) => {
+  try {
+    const response = await fetch(
+      'https://rn-complete-guide-a3ac3.firebaseio.com/orders/u1.json'
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+
+    const loadedOrders = [];
+
+    for (const key in resData) {
+      const { cartItems, date, totalAmount } = resData[key];
+      loadedOrders.push(new Order(key, cartItems, totalAmount, new Date(date)));
+    }
+
+    dispatch({
+      type: SET_ORDERS,
+      orders: loadedOrders,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
 
 export const addOrder = (cartItems, totalAmount) => async (dispatch) => {
   const date = new Date();
--- src/screens/shop/OrdersScreen.js
+++ src/screens/shop/OrdersScreen.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { View, FlatList, Text, StyleSheet } from 'react-native';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { FlatList, ActivityIndicator, View, StyleSheet } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { OrderItem } from '../../components/shop';
+import * as OrdersActions from '../../ducks/ordersDuck';
+import Colors from '../../constants/Colors';
 
 export default function OrdersScreen() {
+  const [isLoading, setItLoading] = useState(false);
+
   const orders = useSelector((state) => state.orders.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(OrdersActions.fetchOrders()).then(() => {
+      setItLoading(false);
+    });
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
 
   return (
     <FlatList
@@ -15,4 +34,10 @@ export default function OrdersScreen() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
